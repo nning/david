f311b95bfddc58ed0700b0fac83fb34a62ec7351
henning mueller <henning@orgizm.net>
2015-01-20 10:59:27 +0100

    Version 0.3.0.

--- lib/david/version.rb
+++ lib/david/version.rb
@@ -2,6 +2,7 @@ module David
   MAJOR = 0
   MINOR = 3
   PATCH = 0
+  SUFFIX = nil
 
-  VERSION = [MAJOR, MINOR, PATCH, 'pre'].join('.')
+  VERSION = [MAJOR, MINOR, PATCH, SUFFIX].compact.join('.')
 end
