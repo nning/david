2e61772b21de868ea5b36d00ad9eabb84293bd6c
henning mueller <henning@orgizm.net>
2015-01-26 09:55:30 +0100

    Use Celluloid pool in Ruby rps benchmark.

--- benchmarks/rps.rb
+++ benchmarks/rps.rb
@@ -2,18 +2,30 @@
 
 require 'benchmark/ips'
 
+require 'celluloid'
+Celluloid.logger = nil
+
 require 'coap'
 include CoRE
 
 uri = ARGV[0] || 'coap://[::1]/hello'
+n = (ARGV[1] || 100).to_i
+
+class Tester
+  include Celluloid
+
+  def run(uri)
+    response = CoAP::Client.new.get_by_uri(uri)
+    raise unless response.mcode == [2, 5]
+  end
+end
 
 Benchmark.ips do |x|
   x.warmup  = 30
   x.time    = 30
 
   x.report(uri) do
-    response = CoAP::Client.new.get_by_uri(uri)
-    raise unless response.mcode == [2, 5]
+    Tester.pool(size: n).run(uri)
   end
 
   x.compare!
