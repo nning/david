6b0fc97d61550ea20f20279291d699f7197d5191
henning mueller <henning@orgizm.net>
2015-02-06 16:11:19 +0100

    config.coap.only=false also disables guerilla Rack handler.

--- lib/david/guerilla/rack/handler.rb
+++ lib/david/guerilla/rack/handler.rb
@@ -14,7 +14,18 @@ module Rack
       elsif ENV.include?("RACK_HANDLER")
         self.get(ENV["RACK_HANDLER"])
       else
-        pick ['david', 'thin', 'puma', 'webrick']
+        # Change original Rack handler order.
+        handlers = ['david', 'thin', 'puma', 'webrick']
+
+        if defined?(Rails)
+          # If Rails is loaded, remove david as first handler if
+          # config.coap.only is set to false.
+          if Rails.application && !Rails.application.config.coap.only
+            handlers = handlers[1..-1]
+          end
+        end
+
+        pick handlers
       end
     end
   end
