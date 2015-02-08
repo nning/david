e5fec7be1862a0413373364543c737bf9f2119ba
henning mueller <henning@orgizm.net>
2015-02-08 16:44:08 +0100

    Use constants instead of symbols for setsockopt (should fix #1).

--- lib/david/server/multicast.rb
+++ lib/david/server/multicast.rb
@@ -3,7 +3,7 @@ module David
     # See https://tools.ietf.org/html/rfc7252#section-12.8
     module Multicast
       def multicast_initialize!
-        @socket.to_io.setsockopt(:SOL_SOCKET, :SO_REUSEADDR, 1)
+        @socket.to_io.setsockopt(Socket::SOL_SOCKET, Socket::SO_REUSEADDR, 1)
 
         if ipv6?
           maddrs = ['ff02::fd', 'ff05::fd']
@@ -28,7 +28,8 @@ module David
 
       def multicast_listen_ipv4(address)
         mreq = IPAddr.new(address).hton + IPAddr.new('0.0.0.0').hton
-        @socket.to_io.setsockopt(:IPPROTO_IP, :IP_ADD_MEMBERSHIP, mreq)
+        @socket.to_io.setsockopt(Socket::IPPROTO_IP, Socket::IP_ADD_MEMBERSHIP,
+          mreq)
       end
 
       def multicast_listen_ipv6(address)
@@ -41,15 +42,17 @@ module David
         end
 
         mreq = IPAddr.new(address).hton + [ifindex].pack('i_')
-        @socket.to_io.setsockopt(:IPPROTO_IPV6, :IPV6_JOIN_GROUP, mreq)
+        @socket.to_io.setsockopt(Socket::IPPROTO_IPV6, Socket::IPV6_JOIN_GROUP,
+          mreq)
       end
 
       def setsockopts_ipv4
-        @socket.to_io.setsockopt(:IPPROTO_IP, :IP_PKTINFO, 1)
+        @socket.to_io.setsockopt(Socket::IPPROTO_IP, Socket::IP_PKTINFO, 1)
       end
 
       def setsockopts_ipv6
-        @socket.to_io.setsockopt(:IPPROTO_IPV6, :IPV6_RECVPKTINFO, 1)
+        @socket.to_io.setsockopt(Socket::IPPROTO_IPV6,
+          Socket::IPV6_RECVPKTINFO, 1)
       end
     end
   end
