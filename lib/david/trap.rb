a4f2a0f25152495490b74d79dbb2abab700bd5aa
henning mueller <henning@orgizm.net>
2015-02-16 13:40:53 +0100

    Make wrapped object optional for Trap.

--- lib/david/trap.rb
+++ lib/david/trap.rb
@@ -1,10 +1,12 @@
-class Trap
-  def initialize(value)
-    @value = value
-  end
+module David
+  class Trap
+    def initialize(value = nil)
+      @value = value
+    end
 
-  def method_missing(method, *args)
-    p method, caller[0]
-    @value.send(method, *args)
+    def method_missing(method, *args)
+      p method, caller[0]
+      @value.send(method, *args) unless @value.nil?
+    end
   end
 end
