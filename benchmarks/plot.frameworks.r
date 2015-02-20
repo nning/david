2061832ce5024b599ed314ae17ba5af5df3aa20a
henning mueller <henning@orgizm.net>
2015-02-20 13:05:57 +0100

    Benchmarks: Plot error bars with standard deviation.

--- benchmarks/plot.frameworks.r
+++ benchmarks/plot.frameworks.r
@@ -15,6 +15,9 @@ Rails = rails$throughput
 df = data.frame(Concurrent, Rack, Rails)
 df.long = melt(df, id.vars='Concurrent')
 
+sd = c(rack$sd, rails$sd)
+df.long = cbind(df.long, sd)
+
 g = ggplot(df.long, aes(Concurrent, value, shape=variable)) +
 	scale_x_log10(breaks=c(10, 50, 100, 500, 1000, 5000, 10000)) +
 	scale_y_continuous(breaks=round(seq(1000, 15000, by=1000), 1)) +
@@ -23,6 +26,7 @@ g = ggplot(df.long, aes(Concurrent, value, shape=variable)) +
 	xlab('Concurrent clients (log.)') +
 	geom_line() +
 	geom_point() +
+ 	geom_errorbar(aes(ymin=value-sd, ymax=value+sd)) +
 	theme_bw()
 
 path = paste(args[1], 'frameworks', 'pdf', sep='.')
--- benchmarks/plot.vms.r
+++ benchmarks/plot.vms.r
@@ -10,17 +10,18 @@ mri22_data   = read.table(file=args[1], sep=',', header=T)
 mri23_data   = read.table(file=args[2], sep=',', header=T)
 jruby17_data = read.table(file=args[3], sep=',', header=T)
 jruby9_data  = read.table(file=args[4], sep=',', header=T)
-# rbx25_data   = read.table(file=args[5], sep=',', header=T)
 
 x       = mri22_data$concurrent
 mri22   = mri22_data$throughput
 mri23   = mri23_data$throughput
 jruby17 = jruby17_data$throughput
 jruby9  = jruby9_data$throughput
-# rbx25   = rbx25_data$throughput
 
-df = data.frame(x, mri22, mri23, jruby17, jruby9) #, rbx25)
+df = data.frame(x, mri22, mri23, jruby17, jruby9)
 df.long = melt(df, id.vars='x')
+
+sd = c(mri22_data$sd, mri23_data$sd, jruby17_data$sd, jruby9_data$sd)
+df.long = cbind(df.long, sd)
 
 g = ggplot(df.long, aes(x, value, shape=variable)) +
 	scale_x_log10(breaks=c(10, 50, 100, 500, 1000, 5000, 10000)) +
@@ -30,6 +31,7 @@ g = ggplot(df.long, aes(x, value, shape=variable)) +
 	xlab('Concurrent clients (log.)') +
 	geom_line() +
 	geom_point() +
+ 	geom_errorbar(aes(ymin=value-sd, ymax=value+sd)) +
 	theme_bw()
 
 path = paste(args[1], 'vms', 'pdf', sep='.')
